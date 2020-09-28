3e41409ab55ce305be9dfa33d6d39bf91a5d3156
CWTBT <lw03232@gmail.com>
2020-09-27 20:44:52 -0500

    Camera/TFlite stuff is now in its own class

---
+++ lib/CameraManager.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:tflite/tflite.dart';
+
+class CameraManager extends StatefulWidget {
+  final CameraDescription camera;
+  const CameraManager({
+    Key key,
+    @required this.camera,
+  }) : super(key: key);
+
+  @override
+  CameraManagerState createState() => CameraManagerState();
+}
+
+class CameraManagerState extends State<CameraManager>{
+  CameraController _controller;
+  Future<void> _initializeControllerFuture;
+  bool isDetecting = false;
+
+  Future<void> loadDetectionModel() async {
+    await Tflite.loadModel(
+        model: "assets/tflite/ssd_mobilenet.tflite",
+        labels: "assets/tflite/ssd_mobilenet.txt"
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = CameraController(
+      widget.camera,
+      ResolutionPreset.high,
+    );
+
+    loadDetectionModel();
+
+    // This is from the example code and I still need to figure out
+    // what it does.
+    _initializeControllerFuture = _controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+
+      _controller.startImageStream((CameraImage img) {
+        if (!isDetecting) {
+          _detectObjects(img);
+        }
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold (
+        appBar: AppBar(title: Text('JanCam')),
+        body: _getCameraPreview()
+    );
+  }
+
+  Widget _getCameraPreview() {
+    return FutureBuilder<void>(
+      future: _initializeControllerFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return CameraPreview(_controller);
+        } else {
+          return Center(child: CircularProgressIndicator());
+        }
+      },
+    );
+  }
+
+  Future<void> _detectObjects(CameraImage img) async {
+    isDetecting = true;
+    Tflite.detectObjectOnFrame(
+
+      bytesList: img.planes.map((plane) {
+        return plane.bytes;
+      }).toList(),
+
+      model: "SSDMobileNet",
+      imageHeight: img.height,
+      imageWidth: img.width,
+      numResultsPerClass: 1,
+
+    ).then((recognitions) {
+      isDetecting = false;
+    });
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,8 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
-import 'package:tflite/tflite.dart';
-
-
+import 'CameraManager.dart';
 
 void main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
   final cameras = await availableCameras();
   final firstCamera = cameras.first;
@@ -15,102 +10,9 @@ void main() async {
   runApp(
       MaterialApp(
         theme: ThemeData.dark(),
-        home: CameraFeed(
+        home: CameraManager(
           camera: firstCamera,
         ),
       ),
   );
-}
-
-class CameraFeed extends StatefulWidget {
-  final CameraDescription camera;
-  const CameraFeed({
-    Key key,
-    @required this.camera,
-  }) : super(key: key);
-
-  @override
-  CameraFeedState createState() => CameraFeedState();
-}
-
-class CameraFeedState extends State<CameraFeed>{
-  CameraController _controller;
-  Future<void> _initializeControllerFuture;
-  bool isDetecting = false;
-
-  loadModel() async {
-    await Tflite.loadModel(
-      model: "assets/tflite/ssd_mobilenet.tflite",
-      labels: "assets/tflite/ssd_mobilenet.txt"
-    );
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = CameraController(
-      widget.camera,
-      ResolutionPreset.low,
-    );
-
-    loadModel();
-    _initializeControllerFuture = _controller.initialize().then((_) {
-      if (!mounted) {
-        return;
-      }
-      setState(() {});
-
-      _controller.startImageStream((CameraImage img) {
-        if (!isDetecting) {
-          _detectObjects(img);
-        }
-      });
-    });
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold (
-      appBar: AppBar(title: Text('JanCam')),
-      body: _getCameraPreview()
-    );
-  }
-
-  Widget _getCameraPreview() {
-    return FutureBuilder<void>(
-      future: _initializeControllerFuture,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          return CameraPreview(_controller);
-        } else {
-          return Center(child: CircularProgressIndicator());
-        }
-      },
-    );
-  }
-
-
-
-  Future<void> _detectObjects(CameraImage img) async {
-    isDetecting = true;
-    Tflite.detectObjectOnFrame(
-      bytesList: img.planes.map((plane) {
-        return plane.bytes;
-      }).toList(),
-      model: "SSDMobileNet",
-      imageHeight: img.height,
-      imageWidth: img.width,
-      numResultsPerClass: 1,
-    ).then((recognitions) {
-      isDetecting = false;
-    });
-  }
-
-
 }
