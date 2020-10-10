f5e3d0d1af9acffb5c20e6ae699d61dfcb5156d5
CWTBT <lw03232@gmail.com>
2020-10-10 14:24:01 -0500

    Fixed the null recognitions nonsense on startup

--- lib/RecognitionDisplay.dart
+++ lib/RecognitionDisplay.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 import 'package:tflite/tflite.dart';
 
+import 'BoundingBox.dart';
 import 'CameraManager.dart';
 
 class RecognitionDisplay extends StatefulWidget {
@@ -14,12 +15,23 @@ class RecognitionDisplay extends StatefulWidget {
 }
 
 class RecognitionDisplayState extends State<RecognitionDisplay> {
+  List<dynamic> _recognitions;
+
+  void setRecognitions(recognitions) {
+    setState(() {
+      _recognitions = recognitions;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold (
-        appBar: AppBar(title: Text('JanCam')),
-        body: new CameraManager(camera: widget.camera),
+        body: Stack (
+          children: [
+            new CameraManager(widget.camera, setRecognitions),
+            new BoundingBox(_recognitions == null ? [] : _recognitions),
+          ]
+        ),
     );
   }
 }
