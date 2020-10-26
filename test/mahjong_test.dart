54ee3dc11b6bdf0c20c19ed51ac995d616055e42
Levi <lw03232@gmail.com>
2020-10-25 20:47:41 -0500

    Added isOpen field to Tile, adjusted sorting based on it

--- test/mahjong_test.dart
+++ test/mahjong_test.dart
@@ -2,26 +2,49 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:jancam/Mahjong.dart';
 
 void main() {
-  test("Sorting Test", () {
+  test("Sorting Fully Closed Hand", () {
     List<Tile> tileList = [
-      Tile(2,"m"),
-      Tile(3,"m"),
-      Tile(4,"m"),
-      Tile(2,"p"),
-      Tile(3,"p"),
-      Tile(4,"p"),
-      Tile(2,"s"),
-      Tile(3,"s"),
       Tile(4,"s"),
+      Tile(1,"p"),
+      Tile(3,"m"),
+      Tile(2,"p"),
       Tile(1,"z"),
-      Tile(1,"z"),
+      Tile(4,"m"),
+      Tile(3,"p"),
+      Tile(2,"m"),
+      Tile(3,"s"),
+      Tile(2,"s"),
       Tile(1,"z"),
       Tile(1,"p"),
-      Tile(1,"p"),
+      Tile(1,"z"),
+      Tile(4,"p"),
     ];
 
     Hand myHand = new Hand(tileList);
     String asString = myHand.toString();
     expect(asString, equals("234m11234p234s111z"));
+  });
+
+  test("Sorting Opened Hand", () {
+    List<Tile> tileList = [
+      Tile(4,"s"),
+      Tile(1,"p"),
+      Tile(2,"m", true),
+      Tile(2,"p"),
+      Tile(1,"z"),
+      Tile(2,"m", true),
+      Tile(3,"p"),
+      Tile(2,"m", true),
+      Tile(3,"s"),
+      Tile(2,"s"),
+      Tile(1,"z"),
+      Tile(1,"p"),
+      Tile(1,"z"),
+      Tile(4,"p"),
+    ];
+
+    Hand myHand = new Hand(tileList);
+    String asString = myHand.toString();
+    expect(asString, equals("11234p234s111z222m"));
   });
 }
